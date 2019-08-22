136d1bd2532467a7d1867a257d217fa19d366a76
unknown <leslalazc@gmail.com>
2019-08-22 16:01:58 -0500

    Function to read files .md and print content links

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const fetch = require("node-fetch");
+
+//function to read an md file
+  fs.readFile(process.argv[2],'utf-8',(err, data) => {
+    if (err) {
+      console.log('error :' , err);
+      return false;
+    }else {
+      console.log("Markdown file is found"); 
+     print_links (data) 
+       return true;
+    }
+  });
+
+   
+ 
+// start the function to validate plain text links with regular expressions
+  let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+
+  function print_links(data){
+    if(regex.test(data)){
+      console.log("content links");
+      console.log(data.match(regex).map(link => link));
+      return true;
+
+    }else {
+      console.log("no content links");
+      return false;
+    }
+  }
+
+  const bringInf = function () {
+    fetch('https://github.com/Lesly-z-c/GDL003-md-links')
+    .then(function(response) {
+      console.log(response.status)
+      return response;
+    }).catch((err) => {
+      console.log(err.message)
+      return err
+    })
+  }
+
+  bringInf();
+
+
+
+
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+/*const mdLinks = require('../');
 
 
 describe('mdLinks', () => {
@@ -8,3 +8,46 @@ describe('mdLinks', () => {
   });
 
 });
+// aqui termina el ejemplo 
+*/
+
+//const mdLinks = require('../index.js');
+/*
+describe('mdLinks', () => {
+
+  it('should return true for a valid .md file', () => {
+    expect(mdLinks('../README.md')).toBe(true);
+    console.log('FIX ME!');
+  });
+
+});
+
+
+//const FileMd = require('../index.js');
+const leerLinks = require('../index.js');
+
+ describe('ReadFile', () => {
+   it('should read the second argument written in the terminal', () => {
+     expect(leerLinks(null, "hola")).toBe(true);
+   })
+ })
+
+ */
+// test que valida .md por callback, pero con function inutil 
+/*
+ const leerLinks = require('../index.js');
+
+ describe('ReadFile', () => {
+   it('should read the second argument written in the terminal', () => {
+     expect(leerLinks(null, "hola")).toBe(true);
+   })
+ })
+*/
+
+const validate_Links = require('../index.js');
+
+describe('validate Links', () => {
+  it('Should validate active or inactive links', () => {
+    expect(validate_Links(true, false)).toBe(true);
+  })
+})
