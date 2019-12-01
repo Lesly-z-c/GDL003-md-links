f613137909d088fb784da8c39afe3fa829a3a55a
unknown <leslalazc@gmail.com>
2019-12-01 17:07:34 -0600

    conflicts are resolved in index map

--- index.js
+++ index.js
@@ -1,88 +1,49 @@
 const fs = require('fs');
 const fetch = require("node-fetch");
-const pathName = process.argv[2];
-const directory = process.argv;
-//let regex = /https?:\S+\w/g
 
-const readPath = (pathName) => {
-  return new Promise(function(reject, resolve) {
-  fs.readFile(pathName,'utf-8',(err, data) => {
+//function to read an md file
+  fs.readFile(process.argv[2],'utf-8',(err, data) => {
     if (err) {
-      reject(err);
-    }else if(data.length > 0){
-        resolve  
-      }else{
-        resolve(null);
-      }
-    
+      console.log('error :' , err);
+      return false;
+    }else {
+      console.log("Markdown file is found"); 
+     print_links (data) 
+       return true;
+    }
   });
-})
-}
 
-const obj = {
-    broken: 0,
-    ok: 0,
-}
+   
+ 
+// start the function to validate plain text links with regular expressions
+  let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
 
-const showStats =  () => {
-  setTimeout(() => {
-     console.log( 'validation statistics' , obj)
-}, 3200)
-}
-const validateLinks =  function () {
-    fs.readFile(pathName,'utf-8',(err, data) => {
-    const alllinks = (data.match(/https?:\S+\w/g).map(link => link)); 
-    //console.log('total links found', alllinks.length) 
-    for(let i = 0; i < alllinks.length; i++){
-      fetch(alllinks[i])
+  function print_links(data){
+    if(regex.test(data)){
+      console.log("content links");
+      console.log(data.match(regex).map(link => link));
+      return true;
+
+    }else {
+      console.log("no content links");
+      return false;
+    }
+  }
+
+  const bringInf = function () {
+    fetch('https://github.com/Lesly-z-c/GDL003-md-links')
     .then(function(response) {
-      if(response.status ===200 ){
-        console.log(` ✓ Ok | active ${response.status} | ${alllinks[i]} | \n  ${pathName} |${directory}`)
-        //console.log(Object.keys(alllinks[i]))
-        obj.ok++
-        return data
-        
-      }else if(response.status ===404 )
-        console.log(` ✖ No | broken ${response.status} | ${alllinks[i]} | \n  ${pathName} |${directory}`)
-        obj.broken++
-       
-        showStats()
-        
-      return broken;
-      
-      
+      console.log(response.status)
+      return response;
     }).catch((err) => {
-      obj.broken++
       console.log(err.message)
       return err
     })
-    }})
-}
-/*
- const printLinks = (data) => {
-  return new Promise(function(reject, resolve){
-if(regex.test(data)){
-const alllinks= ( data.match(regex).map(link => link));
-validate_links(alllinks);  
-return resolve
-}else{
-  console.log('no content links ');
-  return reject
-}
-}
+  }
 
-  )}
-  
-
- 
-readPath(pathName).then(() => {
-  printLinks()+
-})
-*/
-validateLinks();
-readPath(pathName);
+  bringInf();
 
 
 
 
-module.exports = readPath, validateLinks, obj;
+
